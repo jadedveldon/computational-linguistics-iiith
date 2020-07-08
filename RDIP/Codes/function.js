34b002c475e1a6878b6631b6cff38bc6716443c3
jadedveldon <sapan.mathur@gmail.com>
2020-07-09 01:38:04 +0530

    Understanding the corpus requirements

---
+++ RDIP/Codes/function.js
@@ -0,0 +1,22 @@
+﻿var sentanceEnglish = [
+    ['John', 'ate', 'an', 'apple', 'before', 'afternoon'],
+    ['some', 'students', 'like', 'to', 'study', 'at', 'night'],
+    ['John', 'and', 'Mary', 'went', 'to', 'church'],
+    ['John', 'went', 'to', 'church', 'after', 'eating'],
+    ['did', 'he', 'go', 'to', 'market'],
+    ['the', 'woman', 'who', 'called', 'my', 'sister', 'sells', 'cosmetics'],
+    ['John', 'goes', 'to', 'the', 'library', 'and', 'studies'],
+    ['John', 'ate', 'an', 'apple', 'so', 'did', 'she'],
+    ['the', 'teacher', 'returned', 'the', 'book', 'after', 'she', 'noticed', 'the', 'error'],
+    ['I', 'told', 'her', 'that', 'I', 'bought', 'a', 'book', 'yesterday']
+];
+var sentanceHindi = [
+    ['राम', 'और', 'श्याम', 'बाजार', 'गयें'],
+    ['राम', 'सोया', 'और', 'श्याम', 'भी'],
+    ['मैंने', 'उसे', 'बताया', 'कि', 'राम', 'सो', 'रहा', 'है'],
+    ['राम', 'खाकर', 'सोया'],
+    ['बिल्लियों', 'को', 'मारकर', 'कुत्ता', 'सो', 'गया'],
+    ['एक', 'लाल', 'किताब', 'वहाँ', 'है'],
+    ['एक', 'बड़ी', 'सी', 'किताब', 'वहाँ', 'है']
+];
+document.getElementById("demo").innerHTML = sentanceEnglish[3][2];
