b3afa3397734eb32c9250e5581639ea3e410d672
jadedveldon <sapan.mathur@gmail.com>
2020-07-17 19:14:03 +0530

    Understanding corpus requirements

---
+++ RDIP_2/Codes/function.js
@@ -0,0 +1,11 @@
+﻿english1 = 'The child liked the chocolate';
+english2 = 'She was stopped by the bravest knight';
+english3 = 'Mary baked a cake for his birthday';
+english4 = 'She decorated the cake carefully';
+english5 = 'Mary wore a dress with polka dots';
+
+hindi1 = 'राम ने सीता के लिए फल तोड़ा';
+hindi2 = 'छोटे बच्चे पाठशाला जल्दी आयेंगे';
+hindi3 = 'मेहनत का फल मीठा होता है';
+hindi4 = 'वाह! वह खूबसूरत है';
+hindi5 = 'पेड़ से पत्ते गिर गए';
