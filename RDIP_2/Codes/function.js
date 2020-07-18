942437144ff1c487d961754b833feabecea4634b
jadedveldon <sapan.mathur@gmail.com>
2020-07-18 17:06:38 +0530

    Revert "Make different choice dropdowns in table"
    This reverts commit 7dbc553227b1fff42288f03b76ddbe196043b08c.

--- RDIP_2/Codes/function.js
+++ RDIP_2/Codes/function.js
@@ -29,54 +29,82 @@ function englishSelection() {
     document.getElementById("message").style.visibility = "visible";
     var sentance = document.getElementById("corpusEnglish").value;
     if (sentance == 'a') {
-        makeTableE(english1);
+        document.getElementById("main").innerHTML = "<tr><td>LEXICON</td><td>POS</td><td></td><td></td></tr>";
+        english1 = english1.replace(/[^\w\s]|/g, "");
+        arr = english1.split(" ");
+        for (var i = 0; i < arr.length; i++) {
+            document.getElementById("main").innerHTML += "<tr><td>"+arr[i]+"</td><td> </td><td></td><td></td></tr>";
+        }
     }
     if (sentance == 'b') {
-        makeTableE(english2);
+        document.getElementById("main").innerHTML = "<tr><td>LEXICON</td><td>POS</td><td></td><td></td></tr>";
+        english2 = english2.replace(/[^\w\s]|/g, "");
+        arr = english2.split(" ");
+        for (var i = 0; i < arr.length; i++) {
+            document.getElementById("main").innerHTML += "<tr><td>" + arr[i] + "</td><td> </td><td></td><td></td></tr>";
+        }
     }
     if (sentance == 'c') {
-        makeTableE(english3);
+        document.getElementById("main").innerHTML = "<tr><td>LEXICON</td><td>POS</td><td></td><td></td></tr>";
+        english3 = english3.replace(/[^\w\s]|/g, "");
+        arr = english3.split(" ");
+        for (var i = 0; i < arr.length; i++) {
+            document.getElementById("main").innerHTML += "<tr><td>" + arr[i] + "</td><td> </td><td></td><td></td></tr>";
+        }
     }
     if (sentance == 'd') {
-        makeTableE(english4);
+        document.getElementById("main").innerHTML = "<tr><td>LEXICON</td><td>POS</td><td></td><td></td></tr>";
+        english4 = english4.replace(/[^\w\s]|/g, "");
+        arr = english4.split(" ");
+        for (var i = 0; i < arr.length; i++) {
+            document.getElementById("main").innerHTML += "<tr><td>" + arr[i] + "</td><td> </td><td></td><td></td></tr>";
+        }
     }
     if (sentance == 'e') {
-        makeTableE(english5);
+        document.getElementById("main").innerHTML = "<tr><td>LEXICON</td><td>POS</td><td></td><td></td></tr>";
+        english5 = english5.replace(/[^\w\s]|/g, "");
+        arr = english5.split(" ");
+        for (var i = 0; i < arr.length; i++) {
+            document.getElementById("main").innerHTML += "<tr><td>" + arr[i] + "</td><td> </td><td></td><td></td></tr>";
+        }
     }
 }
 function hindiSelection() {
     document.getElementById("message").style.visibility = "visible";
     var sentance = document.getElementById("corpusHindi").value;
     if (sentance == 'a') {
-        makeTableH(hindi1);
+        document.getElementById("main").innerHTML = "<tr><td>LEXICON</td><td>POS</td><td></td><td></td></tr>";
+        arr = hindi1.split(" ");
+        for (var i = 0; i < arr.length; i++) {
+            document.getElementById("main").innerHTML += "<tr><td>" + arr[i] + "</td><td> </td><td></td><td></td></tr>";
+        }
     }
     if (sentance == 'b') {
-        makeTableH(hindi2);
+        document.getElementById("main").innerHTML = "<tr><td>LEXICON</td><td>POS</td><td></td><td></td></tr>";
+        arr = hindi2.split(" ");
+        for (var i = 0; i < arr.length; i++) {
+            document.getElementById("main").innerHTML += "<tr><td>" + arr[i] + "</td><td> </td><td></td><td></td></tr>";
+        }
     }
     if (sentance == 'c') {
-        makeTableH(arr);
+        document.getElementById("main").innerHTML = "<tr><td>LEXICON</td><td>POS</td><td></td><td></td></tr>";
+        arr = hindi3.split(" ");
+        for (var i = 0; i < arr.length; i++) {
+            document.getElementById("main").innerHTML += "<tr><td>" + arr[i] + "</td><td> </td><td></td><td></td></tr>";
+        }
     }
     if (sentance == 'd') {
-        makeTableH(arr);
+        document.getElementById("main").innerHTML = "<tr><td>LEXICON</td><td>POS</td><td></td><td></td></tr>";
+        arr = hindi4.split(" ");
+        for (var i = 0; i < arr.length; i++) {
+            document.getElementById("main").innerHTML += "<tr><td>" + arr[i] + "</td><td> </td><td></td><td></td></tr>";
+        }
     }
     if (sentance == 'e') {
-        makeTableH(arr);
-    }
-}
-var dropdownE = '<select id="selectPOS"><option value="noun">Noun</option><option value="pronoun">Pronoun</option><option value="conjunction">Conjunction</option><option value="interjection">Interjection</option><option value="verb">Verb</option><option value="determiner">Determiner</option><option value="adjective">Adjective</option><option value="adverb">Adverb</option><option value="preposition">Preposition</option></select>'
-var dropdownH = '<select id="selectPOS"><option value="noun">Noun</option><option value="pronoun">Pronoun</option><option value="conjunction">Conjunction</option><option value="interjection">Interjection</option><option value="verb">Verb</option><option value="determiner">Determiner</option><option value="adjective">Adjective</option><option value="adverb">Adverb</option><option value="postposition">Postposition</option></select>'
-function makeTableE(lang) {
-    lang = lang.replace(/[^\w\s]|/g, "");
-    arr = lang.split(" ");
-    document.getElementById("main").innerHTML = "<tr><td>LEXICON</td><td>POS</td><td></td><td></td></tr>";
-    for (var i = 0; i < arr.length; i++) {
-        document.getElementById("main").innerHTML += "<tr><td>" + arr[i] + "</td><td>"+ dropdownE + "</td><td></td><td></td></tr>";
-    }
-}
-function makeTableH(lang) {
-    arr = lang.split(" ");
-    document.getElementById("main").innerHTML = "<tr><td>LEXICON</td><td>POS</td><td></td><td></td></tr>";
-    for (var i = 0; i < arr.length; i++) {
-        document.getElementById("main").innerHTML += "<tr><td>" + arr[i] + "</td><td>"+ dropdownH +"</td><td></td><td></td></tr>";
+        document.getElementById("main").innerHTML = "<tr><td>LEXICON</td><td>POS</td><td></td><td></td></tr>";
+        arr = hindi5.split(" ");
+        for (var i = 0; i < arr.length; i++) {
+            document.getElementById("main").innerHTML += "<tr><td>" + arr[i] + "</td><td> </td><td></td><td></td></tr>";
+        }
     }
 }
