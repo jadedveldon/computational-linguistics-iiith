63020c343f1f4a9c6b422dd1beb31cb61f7a9130
jadedveldon <sapan.mathur@gmail.com>
2020-07-21 22:56:12 +0530

    Install and running POS tagger in your library

--- RDIP_2/Codes/function.js
+++ RDIP_2/Codes/function.js
@@ -84,3 +84,13 @@ function makeTableH(lang) {
     }
     document.getElementById("submitButton").innerHTML = "<button>Submit</button>";
 }
+var pos = require('pos');
+var words = new pos.Lexer().lex('This is some sample text. This text can contain multiple sentences.'); //You have to enter the sentences here
+var tagger = new pos.Tagger();
+var taggedWords = tagger.tag(words);
+for (i in taggedWords) {
+    var taggedWord = taggedWords[i];
+    var word = taggedWord[0];
+    var tag = taggedWord[1];
+    console.log(word + " /" + tag);
+}
